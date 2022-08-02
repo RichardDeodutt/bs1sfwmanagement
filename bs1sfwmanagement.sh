c371858df9aa758c16d88af9644e77d72dca3088
Richard Deodutt <richarddeodutt@gmail.com>
2022-08-02 00:59:50 -0400

    Removed unneeded code, date on filename, added check for up to date

--- bs1sfwmanagement.sh
+++ bs1sfwmanagement.sh
@@ -3,12 +3,13 @@
 #Richard Deodutt
 #08/01/2022
 #This script is a software management script meant for a server. It will automtically update a server using apt package manager when executed. Run with admin permissions when needed using cron to schedule the updates. The only parameter accepted is a file location for where the log file should be stored. 
-#0 23 * * 5 richard sudo /bin/bash ~/bs1sfwmanagement.sh ~/.bs1sfwmanagement.log
+#0 23 * * 5 richard sudo /bin/bash /home/richard/bs1sfwmanagement.sh
 
 
 
 #Logfile location
-LogFile="$HOME/.bs1sfwmanagement.log"
+Date=$(echo "_"`date +"%m-%d-%Y"`)
+LogFile="$HOME/bs1sfwmanagement$Date.log"
 
 #function to get a timestamp
 timestamp(){
@@ -27,7 +28,7 @@ setuplogfile(){
     #Check if log file creation worked
     if [ ! $? -eq 0 ]; then
         #if the previous log file creation failed try one last time
-        LogFile=".bs1sfwmanagement.log"
+        LogFile="bs1sfwmanagement$Date.log"
         touch $LogFile > /dev/null 2>&1
     fi
 }
@@ -40,22 +41,16 @@ setuplogfile(){
 
 #Check if we have atleast 1 argument
 if [ $# -gt 0 ]; then 
-    #Assuming the first argument is a log file, check if it exists
-    if [ -f "$1" ]; then
-        #Argument log file exists, using this file as a log file
+    #Assuming the first argument is a log file, check if it exists or create it and hide command errors
+    touch $1 > /dev/null 2>&1
+    #Check if argument logfile exists or creation worked
+    if [ $? -eq 0 ]; then
+        #Argument log file exists/creation worked, using this file as a log file
         LogFile=$1
     else
-        #Argument log file does not exists, attempt to create it and hide command errors
-        touch $1 > /dev/null 2>&1
-        #Check if argument log file creation worked
-        if [ $? -eq 0 ]; then
-            #Argument log file creation worked, using this file as a log file
-            LogFile=$1
-        else
-            #Set up logfile and log error with the arugment
-            setuplogfile
-            log "Could not create log file: $1"
-        fi
+        #Set up logfile and log error with the arugment
+        setuplogfile
+        log "Could not create log file: $1"
     fi
 fi
 
@@ -69,4 +64,11 @@ log "For this run the log file location is: $LogFile"
 if [ $UID != 0 ]; then
     log "This script was not run with admin permissions, run it again with admin permissions"
     exit 1
+fi
+
+Update=$(apt update)
+Latest=$(echo $Update | grep -c "All packages are up to date.")
+
+if [ $Latest -eq 1 ]; then
+    log "All packages are up to date."
 fi
